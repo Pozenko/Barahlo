6b8833d0e6592cd85f8805db06357c7679506b04
Pozenko <pozenko@imac-artem-pozenko.local>
2017-11-02 17:56:17 +0300

    add filter categories

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -15,7 +15,7 @@
                     <div class="form-group col-lg-4 col-md-4">
                         <label for="selectCategories">Катерогии</label>
                         <select class="form-control" id="selectCategories">
-                            <option value="">Выберите категорию</option>
+                            <option value="">Выберите категорию...</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
@@ -24,19 +24,28 @@
                     <div class="form-group col-lg-4 col-md-4">
                         <label for="selectSubCategories">Подкатегории</label>
                         <select class="form-control" id="selectSubCategories">
-                            <option value="">Выберите подкатегорию</option>
+                            <option value="">Выберите подкатегорию...</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                         </select>
                     </div>
-                    <div class="form-group col-lg-4 col-md-4">
+                    <div class="form-group col-lg-2 col-md-2">
                         <label for="selectSellingOption">Тип сделки</label>
                         <select class="form-control" id="selectSellingOption">
-                            <option value="">Тип</option>
+                            <option value="">Тип...</option>
                             <option value="1">Покупка</option>
                             <option value="2">Продажа</option>
                             <option value="3">Обмен</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-2 col-md-2">
+                        <label for="selectCity">Город</label>
+                        <select class="form-control" id="selectCity">
+                            <option value="">Выберите город...</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
                         </select>
                     </div>
                 </div>
