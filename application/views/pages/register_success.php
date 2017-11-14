a6b6dff2ebacb41b92f5cf269f1c24f24b597363
Pozenko <pozenko@imac-artem-pozenko.local>
2017-11-14 20:14:58 +0300

    correct register_success

--- application/views/pages/register_success.php
+++ application/views/pages/register_success.php
@@ -1,13 +1,15 @@
-<div class="row my_row_bg">
-    <div class="col-lg-2">
+<div class="container">
+    <div class="row my_row_bg">
+        <div class="col-lg-2">
 
-    </div>
-    <div class="my_center_container col-lg-8">
-        <h3>Регистрация прошла успешно!</h3>
-        <p><?php echo anchor('register', 'Заполнить еще раз!'); ?></p>
-    </div>
-    <div class="col-lg-2">
+        </div>
+        <div class="my_center_container col-lg-8">
+            <h3>Регистрация прошла успешно!</h3>
+            <p><?php echo anchor('register', 'Заполнить еще раз!'); ?></p>
+        </div>
+        <div class="col-lg-2">
 
+        </div>
     </div>
 </div>
 
